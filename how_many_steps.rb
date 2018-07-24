0119dbca1e0d1432bc25fbee9c38ad9808d848ce
amulyamari <amulya@saimail.com>
2018-07-24 21:02:07 +0000

    automatically backed up by learn

--- how_many_steps.rb
+++ how_many_steps.rb
@@ -1,15 +1,15 @@
 def how_many_steps
   steps=0
   loop do
-  puts steps
-  steps+=1
-  if steps % 2==0
-    puts "left"
-  else 
-    puts "right" 
-  end   
-  sleep(0.5)
-  break
+    puts steps
+    steps+=1
+      if steps % 2==0
+        puts "left"
+      else 
+        puts "right" 
+      end   
+    sleep(0.5)
+    break
   end
 end
 
