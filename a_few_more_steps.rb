ae2d840fa8b13f1d6fefb2ecb22c35ba7a92c3f5
amulyamari <amulya@saimail.com>
2018-07-24 20:41:41 +0000

    automatically backed up by learn

---
+++ a_few_more_steps.rb
@@ -0,0 +1,21 @@
+def first_steps
+    loop do
+    sleep(0.5)
+    puts "left foot back"
+    sleep(0.5)
+    puts "right foot back"
+    sleep(0.5)
+    puts "stop"
+    sleep(1)
+    puts "right foot steps right and back"
+    sleep(0.5)
+    puts "left foot crosses over right"
+    sleep (0.5)
+    puts "right foot steps right"
+    sleep (0.5)
+    puts "turn"
+    sleep (1)
+    break
+  end
+end
+first_steps 
